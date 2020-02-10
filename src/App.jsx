a3e4f853cee72dbaff3e00fb24cdf438a144a280
Dan McKinney <3dhdaniel@gmail.com>
2020-02-10 15:13:05 -0500

    this course no beuno

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,29 @@
-ReactDOM.render(
-    <h1>Hello, world</h1>,
+
+
+class Todo extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { done: (this.props.done == 'true' && props.done),
+                        text: props.text}
+                        this.handleClick = this.handleClick.bind(this)
+    }
+    
+    handleClick(event){
+        this.setState(state => {
+            done: !state.done
+        })
+    }
+    render() {
+        return <div className="todo">
+                <span>
+                  <input type="checkbox" checked={this.state.done} onClick={this.handleClick}/>
+                  <input type='text' value={this.state.text}/>
+                  </span>
+             </div>;
+    }
+  }
+
+  ReactDOM.render(
+    <Todo  text='Todo 1' done='false' />,
     document.getElementById('root')
-)
+  );
